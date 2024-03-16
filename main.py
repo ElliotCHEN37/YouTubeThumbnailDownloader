dcf1336d62529a3dd91d23b229c20618f1624a30
Elliot <elliotchen2010@gmail.com>
2024-03-16 11:16:25 +0800

    fix errors

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import sys
 import requests
-from PyQt5.QtGui import QIcon
+from PyQt5 import QtGui, QtWidgets
 from pytube import YouTube
-from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox
 from ui_main import Ui_MainWindow
 
-class MainWindow(QMainWindow):
+class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        self.setWindowIcon(QIcon(':/img/resources/YTTDL-LOGO.png'))
+        self.setWindowIcon(QtGui.QIcon(':/img/resources/YTTDL-LOGO.png'))
         self.ui.pushButton_dl.clicked.connect(self.download_thumbnail)
 
     def download_thumbnail(self):
@@ -22,21 +21,21 @@ class MainWindow(QMainWindow):
 
             response = requests.get(thumbnail_url)
             if response.status_code == 200:
-                save_path, _ = QFileDialog.getSaveFileName(self, self.tr("Save Thumbnail"), "", self.tr("Images (*.png *.jpg)"))
+                save_path, _ = QtWidgets.QFileDialog.getSaveFileName(self, self.tr("Save Thumbnail"), "", self.tr("Images (*.png *.jpg)"))
                 if save_path:
                     with open(save_path, 'wb') as f:
                         f.write(response.content)
                     print("Thumbnail downloaded successfully at:", save_path)
-                    QMessageBox.information(self, self.tr("Success"), self.tr("Thumbnail downloaded successfully at: ") + save_path)
+                    QtWidgets.QMessageBox.information(self, self.tr("Success"), self.tr("Thumbnail downloaded successfully at: ") + save_path)
                 else:
                     print("Failed to download thumbnail.")
-                    QMessageBox.critical(self, self.tr("Warning"), self.tr("Failed to download thumbnail."))
+                    QtWidgets.QMessageBox.critical(self, self.tr("Warning"), self.tr("Failed to download thumbnail."))
         except Exception as e:
             print("Error downloading thumbnail:", str(e))
-            QMessageBox.critical(self, self.tr("Error"), self.tr("Error downloading thumbnail: ") + str(e))
+            QtWidgets.QMessageBox.critical(self, self.tr("Error"), self.tr("Error downloading thumbnail: ") + str(e))
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
