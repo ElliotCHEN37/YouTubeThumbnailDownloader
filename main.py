d44e04a4f1ab753d08e1da93b104095153bdc1d5
Elliot <elliotchen2010@gmail.com>
2024-03-16 12:13:50 +0800

    [Release] v2.1

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 import requests
-from PyQt5 import QtGui, QtWidgets
+from PyQt5 import QtWidgets, QtGui
 from pytube import YouTube
 from ui_main import Ui_MainWindow
 
@@ -13,6 +13,10 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setWindowIcon(QtGui.QIcon(':/img/resources/YTTDL-LOGO.png'))
         self.ui.pushButton_dl.clicked.connect(self.download_thumbnail)
 
+        self.ui.actionPaste_YouTube_Video_Link.triggered.connect(self.paste_youtube_video_link)
+        self.ui.actionExit.triggered.connect(self.close)
+        self.ui.actionAbout.triggered.connect(self.about)
+
     def download_thumbnail(self):
         try:
             youtube_url = self.ui.Lineedit_URL.text()
@@ -21,18 +25,26 @@ class MainWindow(QtWidgets.QMainWindow):
 
             response = requests.get(thumbnail_url)
             if response.status_code == 200:
-                save_path, _ = QtWidgets.QFileDialog.getSaveFileName(self, self.tr("Save Thumbnail"), "", self.tr("Images (*.png *.jpg)"))
+                save_path, _ = QtWidgets.QFileDialog.getSaveFileName(self, "Save Thumbnail", "", "Images (*.png *.jpg)")
                 if save_path:
                     with open(save_path, 'wb') as f:
                         f.write(response.content)
                     print("Thumbnail downloaded successfully at:", save_path)
-                    QtWidgets.QMessageBox.information(self, self.tr("Success"), self.tr("Thumbnail downloaded successfully at: ") + save_path)
+                    QtWidgets.QMessageBox.information(self, "Success", "Thumbnail downloaded successfully at: " + save_path)
                 else:
                     print("Failed to download thumbnail.")
-                    QtWidgets.QMessageBox.critical(self, self.tr("Warning"), self.tr("Failed to download thumbnail."))
+                    QtWidgets.QMessageBox.critical(self, "Warning", "Failed to download thumbnail.")
         except Exception as e:
             print("Error downloading thumbnail:", str(e))
-            QtWidgets.QMessageBox.critical(self, self.tr("Error"), self.tr("Error downloading thumbnail: ") + str(e))
+            QtWidgets.QMessageBox.critical(self, "Error", "Error downloading thumbnail: " + str(e))
+
+    def paste_youtube_video_link(self):
+        clipboard = QtWidgets.QApplication.clipboard()
+        self.ui.Lineedit_URL.setText(clipboard.text())
+        self.download_thumbnail()
+
+    def about(self):
+        QtWidgets.QMessageBox.about(self, "About", "YouTube Thumbnail Downloader v2.1 by ElliotCHEN37\n\nThis program was licensed under MIT License")
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
